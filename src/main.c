f64f80cce75f9e2eda7b0a7507e902d60b02e1a2
Matt Sherwood <msherwood21@gmail.com>
2023-12-28 10:56:24 -0600

    Updated resolution, colors and text coordinates

--- src/main.c
+++ src/main.c
@@ -1,36 +1,52 @@
+#include <graphics.h>
 #include <libdragon.h>
 
-enum BinaryColor {
-    Black = 0x00000000,
-    White = 0xffffffff
-};
+//-
+//- Static Data
+//-
 
-void drawAcknowledgement(display_context_t * disp) {
+// No transparency
+static color_t const Black = { 0x00, 0x00, 0x00, 0xff };
+static color_t const White = { 0xff, 0xff, 0xff, 0xff };
+
+//-
+//- Private Functions
+//-
+
+void drawAcknowledgement() {
+    surface_t * display = NULL; // non-owning
+
     // Grab a render buffer
-    while(!(*disp = display_lock()));
+    while(!(display = display_lock()));
 
     // Fill the screen
-    graphics_fill_screen(*disp, Black);
+    graphics_fill_screen(display, graphics_convert_color(Black));
 
     // Set the text color
-    graphics_set_color(White, Black);
+    graphics_set_color(graphics_convert_color(White), graphics_convert_color(Black));
 
     // Draw the text
-    graphics_draw_text(*disp, 20, 20, "For Oliver");
+    graphics_draw_text(display, 290, 240, "For Oliver");
 
-    // Force backbuffer flip?
-    display_show(*disp);
+    // Draw to our buffer
+    display_show(display);
 }
 
 int main(void) {
-    display_init(RESOLUTION_320x240, DEPTH_16_BPP, 2, GAMMA_NONE, FILTERS_RESAMPLE);
+    static uint32_t const BufferCount = 2;
 
-    static display_context_t disp = 0;
+    display_init(
+        RESOLUTION_640x480, // interlaced
+        DEPTH_16_BPP,
+        BufferCount,
+        GAMMA_NONE,
+        FILTERS_DISABLED
+    );
 
-    drawAcknowledgement(&disp);
+    drawAcknowledgement();
 
     // Main loop
-    while (1) {}
+    while (true) {}
 
     return 0;
 }
