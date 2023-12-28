1673088bd279c67a3d14813e39d0035bf8df439f
Matt Sherwood <msherwood21@gmail.com>
2023-12-28 11:51:55 -0600

    Added square drawing test. Display resolution and technique changed to fix display jittering.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <graphics.h>
 #include <libdragon.h>
+#include <timer.h>
 
 //-
 //- Static Data
@@ -7,11 +8,81 @@
 
 // No transparency
 static color_t const Black = { 0x00, 0x00, 0x00, 0xff };
+static color_t const Grey = { 0x80, 0x80, 0x80, 0xff };
 static color_t const White = { 0xff, 0xff, 0xff, 0xff };
+static color_t const Red = { 0xff, 0x00, 0x00, 0xff };
+static color_t const Green = { 0x00, 0xff, 0x00, 0xff };
+static color_t const Blue = { 0x00, 0x00, 0xff, 0xff };
+static color_t const Yellow = { 0xff, 0xff, 0x00, 0xff };
 
 //-
 //- Private Functions
 //-
+
+void resetBoard(int ovfl) {
+    surface_t * display = NULL; // non-owning
+
+    // Grab a render buffer
+    while(!(display = display_lock()));
+
+    // Fill the screen
+    graphics_fill_screen(display, graphics_convert_color(Black));
+
+    // Draw selector
+    graphics_draw_box(
+        display,
+        10,
+        5,
+        50,
+        40,
+        graphics_convert_color(White)
+    );
+
+    // Draw squares
+    graphics_draw_box(
+        display,
+        15,
+        10,
+        40,
+        30,
+        graphics_convert_color(Red)
+    );
+    graphics_draw_box(
+        display,
+        65,
+        10,
+        40,
+        30,
+        graphics_convert_color(Green)
+    );
+    graphics_draw_box(
+        display,
+        115,
+        10,
+        40,
+        30,
+        graphics_convert_color(Blue)
+    );
+    graphics_draw_box(
+        display,
+        165,
+        10,
+        40,
+        30,
+        graphics_convert_color(Yellow)
+    );
+    graphics_draw_box(
+        display,
+        215,
+        10,
+        40,
+        30,
+        graphics_convert_color(Grey)
+    );
+
+    // Draw to our buffer
+    display_show(display);
+}
 
 void drawAcknowledgement(void) {
     surface_t * display = NULL; // non-owning
@@ -26,7 +97,7 @@ void drawAcknowledgement(void) {
     graphics_set_color(graphics_convert_color(White), graphics_convert_color(Black));
 
     // Draw the text
-    graphics_draw_text(display, 290, 240, "For Oliver");
+    graphics_draw_text(display, 125, 120, "For Oliver");
 
     // Draw to our buffer
     display_show(display);
@@ -35,15 +106,23 @@ void drawAcknowledgement(void) {
 int main(void) {
     static uint32_t const BufferCount = 2;
 
+    // Never freed
     display_init(
-        RESOLUTION_640x480, // interlaced
+        RESOLUTION_320x240, // progressive (TODO: investigate interlaced jitter)
         DEPTH_16_BPP,
         BufferCount,
         GAMMA_NONE,
-        FILTERS_DISABLED
+        FILTERS_RESAMPLE
     );
+    timer_init();
 
     drawAcknowledgement();
+
+    new_timer(
+        TIMER_TICKS(5000000),   // 5 seconds
+        TF_ONE_SHOT,
+        resetBoard
+    );
 
     // Main loop
     while (true) {}
